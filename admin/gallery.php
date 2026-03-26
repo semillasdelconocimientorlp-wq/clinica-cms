69b54e5adca46acadd7b248b397faacc6b5f5d54
semillasdelconocimientorlp-wq <semillasdelconocimientorlp@gmail.com>
2026-03-26 18:15:56 -0400

    Adding admin gallery.php file with image gallery management system

---
+++ admin/gallery.php
@@ -0,0 +1,16 @@
+<!-- Image Gallery Management System -->
+<html>
+<head>
+    <title>Image Gallery</title>
+</head>
+<body>
+    <h1>Image Gallery Management System</h1>
+    <div id="gallery">
+        <!-- Gallery Images will be displayed here -->
+    </div>
+
+    <script>
+        // JavaScript to manage image gallery goes here
+    </script>
+</body>
+</html>
